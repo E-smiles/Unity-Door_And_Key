8a0edfb68e3bd04c7b5b655cedc9905bcda3532e
Scheherazad Zourhbat <scheherazad.zourhbat@gmail.com>
2022-09-16 15:02:47 +0200

    Added scripts

---
+++ Assets/#Projet/Scripts/Door.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Pour lui dire que si il y a un animator il doit le trouver
+[RequireComponent(typeof(Animator))]
+public class Door : MonoBehaviour
+{
+    [HideInInspector] public LevelManager manager;
+    [HideInInspector] public bool isOpen = false;
+    private Animator animator;
+
+    void Start(){
+        //Va chercher le composant entre les chevrons et vous le ramène
+        animator = GetComponent<Animator>();
+    }
+
+    public void Opening(){
+        isOpen = true;
+        //Elle gère l'animator devient true(certain que l'animation va se jouer)
+        animator.SetBool("DoorIsOpen", true);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other){
+        //Si je suis bien devant un game objet player alors mon propre game object se désactive
+        if(other.CompareTag("Player")){
+            gameObject.SetActive(false);
+            manager.keyIsReached();
+        }
+
+    }
+}
---
+++ Assets/#Projet/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    //On déclare une key de type Key
+    public Key key;
+    public Door door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Si il ne connait pas la clé, on lui met une erreur
+        if(key == null){
+            Debug.LogError("LevelManager need a key.", gameObject);
+        }
+        else{
+            //Le manager de cette clé c'est moi même.
+            key.manager = this;
+        }
+
+        if(door == null){
+            Debug.LogError("LevelManager need a door.", gameObject);
+        }
+        else{
+            //Le manager de cette porte c'est moi même.
+            door.manager = this;
+        }
+
+    }
+
+    public void keyIsReached(){
+        door.Opening();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
