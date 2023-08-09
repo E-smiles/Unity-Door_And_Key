a3f05030e6bf1dc922e6adcce9e9e7b0108613bd
Scheherazad Z <scheherazad.zourhbat@gmail.com>
2023-08-09 17:35:18 +0200

    add jump

--- Assets/#Projet/Scripts/HeroInputManager.cs
+++ Assets/#Projet/Scripts/HeroInputManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-//using System.Debug;
+//using System.Debug.Log;
 
 //On lui dit qu'on a besoin d'un rigid body (etre sur qu'il soit deja la)
 [RequireComponent(typeof(Rigidbody2D))]
@@ -21,6 +21,7 @@ public class HeroInputManager : MonoBehaviour
     private SpriteRenderer mySprite;
     private bool IsJumping = false;
     public float jumpforce = 20;
+    public int nbrOfJump = 1;
 
     bool isGrounded;
     public Transform groundCheck;
@@ -56,6 +57,21 @@ public class HeroInputManager : MonoBehaviour
     }
     void JumpStart(InputAction.CallbackContext context){
     IsJumping = true;
+        if(isGrounded)
+        {
+            nbrOfJump = 0;
+            rb.AddForce(Vector2.up*jumpforce);
+            nbrOfJump++;
+        }
+        else
+        {
+            if(nbrOfJump == 1)
+        {
+            //rb.AddForce(Vector2.up*jumpforce);
+            nbrOfJump--;
+        }
+
+        }
     }
 
     void JumpCancel(InputAction.CallbackContext context){
@@ -74,7 +90,7 @@ void FixedUpdate()
 {
     //Je vais regarder grace à la fct overlapcircle si mon empty est bien sur le sol (je dois passer trois parametres car mon bool va toujours me renvoyer true si je ne mets pas un layer masque)
     isGrounded = Physics2D.OverlapCircle(groundCheck.position, 1, groundLayer);
-    //Debug.log(isGrounded);
+    // J'essaye de faire un debug, mais il ne fonctionne pas Debug.log(isGrounded);
 
     //Je vais recupérer le vector2 de mon movaction, il va me donner le déplacement que je vais vouloir faire
     Vector2 move = moveAction.ReadValue<Vector2>() * Time.fixedDeltaTime * speed;
@@ -102,89 +118,3 @@ void FixedUpdate()
 
 
 }
-
-//Test ISGROUNDED
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.InputSystem;
-
-// [RequireComponent(typeof(Rigidbody2D))]
-// [RequireComponent(typeof(Animator))]
-// [RequireComponent(typeof(SpriteRenderer))]
-// public class HeroInputManager : MonoBehaviour
-// {
-//     private InputManager actions;
-//     private InputAction moveAction;
-//     private InputAction jumpAction;
-//     private Rigidbody2D rb;
-//     private Animator animator;
-//     private SpriteRenderer mySprite;
-//     private bool isJumping = false;
-//     private bool isGrounded = false;
-
-//     [SerializeField] private LayerMask groundLayer;
-//     [SerializeField] private Transform groundCheck;
-//     [SerializeField] private float groundCheckRadius = 0.2f;
-//     public float speed = 3;
-
-//     private void Awake()
-//     {
-//         actions = new InputManager();
-//         moveAction = actions.Player.Move;
-//         moveAction.Enable();
-
-//         actions.Player.Jump.Enable();
-
-//         rb = GetComponent<Rigidbody2D>();
-//         animator = GetComponent<Animator>();
-//         mySprite = GetComponent<SpriteRenderer>();
-//     }
-
-//     private void OnEnable()
-//     {
-//         actions.Player.Jump.started += JumpStart;
-//         actions.Player.Jump.canceled += JumpCancel;
-//     }
-
-//     private void OnDisable()
-//     {
-//         actions.Player.Jump.started -= JumpStart;
-//         actions.Player.Jump.canceled -= JumpCancel;
-//     }
-
-//     private void JumpStart(InputAction.CallbackContext context)
-//     {
-//         if (isGrounded)
-//         {
-//             isJumping = true;
-//             rb.AddForce(Vector2.up * 50);
-//         }
-//     }
-
-//     private void JumpCancel(InputAction.CallbackContext context)
-//     {
-//         isJumping = false;
-//     }
-
-//     private void FixedUpdate()
-//     {
-//         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-//         Vector2 move = moveAction.ReadValue<Vector2>() * Time.fixedDeltaTime * speed;
-//         rb.MovePosition(transform.position + (Vector3)move);
-
-//         float velocity = Mathf.Abs(move.x);
-//         animator.SetFloat("Speed", velocity);
-
-//         if (move.x > 0)
-//         {
-//             mySprite.flipX = false;
-//         }
-//         else if (move.x < 0)
-//         {
-//             mySprite.flipX = true;
-//         }
-//     }
-// }
